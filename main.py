247a924f6c0fbcb2bd652b05be4c032a1c98247c
tofikhidayatxyz <topikhidayat711@gmail.com>
2022-01-22 15:39:50 +0700

    class method

--- main.py
+++ main.py
@@ -1,24 +1,72 @@
+class Tv:
+    channels =  ['Rcti', 'TVri', 'MNC', 'NET.']
+    channes = None
+    brand = None
+    resolution = None
+    isOn = False
 
-import sys
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def setChannel(self, index):
+        if(self.isOn) :
+            self.channel = self.channels[index]
+        else :
+            print('Tv should be on !')
+
+    def addChannel(self, newChannel) :
+        if(self.isOn) :
+            self.channels.append(newChannel)
+        else :
+            print('Tv should be on !')
+    
+    def manualRemove(self, toDelete):
+        if(self.isOn) :
+            try:
+                self.channels.remove(toDelete)
+            except Exception as ex :
+                print(ex)
+        else :
+            print('Tv should be on !')
+       
+    
+    def turnOn(self):
+        self.isOn = True
+
+    def turnOff(self):
+        self.isOn = False
+
+    def removeChannel(self, toDelete):
+        self.channels = list(filter(lambda x : x != toDelete, self.channels))
+
+    # Hiraukan di bawah ini
+    @classmethod
+    def tvSharp(cls, size):
+        print('Class method initialized')
+        return cls('Sharp',  size)
+    # Destruct
+    def __del__(self):
+        print(F'This {self.brand}  object has been deleted')
 
 
-class Car:
-    isCarRun = False
-    @staticmethod
-    def run():
-        if(Car.isCarRun):
-            print('CAR IN RUNING')
-        else:
-            print('CAR IN NOT RUN')
+class SmartTv(Tv):
+    applications = ['youtube', 'playstore', 'netflix', 'video.com']
+    
+    def __init__(self, brand, size):
+        super(SmartTv).__init__(brand, size)
 
-class Lamborgini(Car):
-    def __init__(self):
-        super(Lamborgini).__init__()
-    @staticmethod
-    def gas():
-        try :
-            Lamborgini.runAll()
-        except:
-            print('OOPSSSSSSSS ERROR \n ', sys.exc_info)
-
-Lamborgini.gas()
+# prints
+tvRuangTamu = Tv('LG', '21 inch')
+print(tvRuangTamu.channels)
+tvRuangTamu.setChannel(1)
+tvRuangTamu.turnOn()
+tvRuangTamu.addChannel('ANTV')
+print(tvRuangTamu.channels)
+tvRuangTamu.removeChannel('TVri')
+print(tvRuangTamu.channels)
+tvRuangTamu.removeChannel(1)
+print(tvRuangTamu.channels)
+tvRuangTamu.manualRemove('MNCS')
+tvRuangTamu.turnOff()
+print(tvRuangTamu.__dict__)
