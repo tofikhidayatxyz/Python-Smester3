3c11650917aabc3b61ec35c2041b1c818227b334
tofikhidayatxyz <topikhidayat711@gmail.com>
2022-01-30 21:41:38 +0700

    FIX: uas

--- main.py
+++ main.py
@@ -1,106 +1,146 @@
-import hashlib
-from xmlrpc.client import boolean
+# Nama: Tofik Hidayat
+# NIM: 20200040056
+# Kelas: TI 20C
 
-# class Tv:
-#     channels =  ['Rcti', 'TVri', 'MNC', 'NET.']
-#     channes = None
-#     brand = None
-#     resolution = None
-#     isOn = False
+class Tv:
+    channels =  ['MNC', 'NET', 'TVRI', 'RAC']
+    channel = None
+    brand = None
+    resolution = None
+    isOn = False
 
-#     def __init__(self, brand, size):
-#         self.brand = brand
-#         self.size = size
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
 
-#     def setChannel(self, index):
-#         if(self.isOn) :
-#             self.channel = self.channels[index]
-#         else :
-#             print('Tv should be on !')
+    def setChannel(self, index):
+        print('Set Channel TV')
+        if(self.isOn) :
+            self.channel = self.channels[index]
+        else :
+            print('Tv should be on !')
 
-#     def addChannel(self, newChannel) :
-#         if(self.isOn) :
-#             self.channels.append(newChannel)
-#         else :
-#             print('Tv should be on !')
+    def addChannel(self, newChannel) :
+        if(self.isOn) :
+            self.channels.append(newChannel)
+        else :
+            print('Tv should be on !')
     
-#     def manualRemove(self, toDelete):
-#         if(self.isOn) :
-#             try:
-#                 self.channels.remove(toDelete)
-#             except Exception as ex :
-#                 print(ex)
-#         else :
-#             print('Tv should be on !')
-       
+    def manualRemove(self, toDelete):
+        print('Remove Channel TV')
+        if(self.isOn) :
+            try:
+                self.channels.remove(toDelete)
+            except Exception as ex :
+                print(ex)
+        else :
+            print('Tv should be on !')
     
-#     def turnOn(self):
-#         self.isOn = True
+    def turnOn(self):
+        print('Turn On TV')
+        self.isOn = True
 
-#     def turnOff(self):
-#         self.isOn = False
+    def turnOff(self):
+        print('Turn Off TV')
+        self.isOn = False
 
-#     def removeChannel(self, toDelete):
-#         self.channels = list(filter(lambda x : x != toDelete, self.channels))
-
-#     # Hiraukan di bawah ini
-#     @classmethod
-#     def tvSharp(cls, size):
-#         print('Class method initialized')
-#         return cls('Sharp',  size)
-#     # Destruct
-#     def __del__(self):
-#         print(F'This {self.brand}  object has been deleted')
-
-# # Static method
-#     @staticmethod
-#     def createTv():
-#         return Tv('LG', '21 inch')
+    def removeChannel(self, toDelete):
+        print('Remove Channel TV')
+        self.channels = list(filter(lambda x : x != toDelete, self.channels))
 
 
-# # prints
-# tvRuangTamu = Tv.createTv()
-# print(tvRuangTamu.channels)
-# tvRuangTamu.setChannel(1)
-# tvRuangTamu.turnOn()
-# tvRuangTamu.addChannel('ANTV')
-# print(tvRuangTamu.channels)
-# tvRuangTamu.removeChannel('TVri')
-# print(tvRuangTamu.channels)
-# tvRuangTamu.removeChannel(1)
-# print(tvRuangTamu.channels)
-# tvRuangTamu.manualRemove('MNCS')
-# tvRuangTamu.turnOff()
-# print(tvRuangTamu.__dict__)
+tvs = []
+
+
+tvs.append(Tv('Sharp', '12'))
+
+def addnewTv():
+    brand = input('Isi Brand : ')
+    size = input('Isi Ukuran (Inch) : ')
+
+    tvs.append(Tv(brand, size))
+
+def viewTv():
+    for index, item in enumerate(tvs):
+        print('--------' , (index + 1) , '-----------')
+        print('Brand ', item.brand)
+        print('Ukuran ', item.resolution)
+        print('TV sedang ' , "Menyala" if item.isOn else "Mati")
+        print('Channel TV : ' , item.channels)
+        print('Channel Aktif : ' , item.channel)
+        print("------------------------")
+
+def selectTv():
+    print('----------------------')
+    index = int(input('Pilih Nomor Tv :'))
+    try :
+        tv = tvs[index - 1]
+        while(True):
+            print('----------------------')
+            print('Pilih Menu Dari TV ', tv.brand , ': ')
+            print('a. Nyalakan')
+            print('b. Matikan TV')
+            print('c. Ganti Channel')
+            print('d. Hapus Channel')
+            print('[na]. Back')
+
+            subMenu = input('Pilih Menu : ')
+
+            try:
+                if(subMenu == 'a'):
+                    tv.turnOn()
+                elif(subMenu == 'b'):
+                    tv.turnOff()
+                elif(subMenu == 'c'):
+                    print('Available channels : ', tv.channels)
+                    channelIdx = int(input('Masukan Nomor Channel : '))
+                    tv.setChannel(channelIdx - 1)
+                elif(subMenu == 'd'):
+                    print('Available channels : ', tv.channels)
+                    channelIdx = int(input('Masukan Index Channel : '))
+                    del tv.channels[channelIdx]
+                else:
+                    break  
+            except Exception as e:
+                print(e) 
+    except Exception as e:
+        print(e)
+        print('Tv not found')
+
+def removeTv():
+    print('--------- HAPUS -----------')
+    try :
+        tvIdx = int(input('Masukan Index TV : '))
+        del tvs[tvIdx - 1]
+        print("TV Terhapus")
+    except Exception as e:
+        print(e)
 
 
 
-class User:
-    __password = None
-    def __init__(self) -> None:
-        pass
+while(True):
+    print('Pilih Menu : ')
+    print('1. Tambah TV')
+    print('2. Lihat Daftar TV')
+    print('3. Pilih TV')
+    print('4. Hapus TV')
+    print('[na]. End')
 
-    def _setPassword(self, newPassword):
-        self.__password = hashlib.sha256(str(newPassword).encode('utf-8')).hexdigest()
+    menu = input('Pilih Menu : ')
 
-    def _comparePassword(self, toCompare) -> boolean:
-        return self.__password == hashlib.sha256(str(toCompare).encode('utf-8')).hexdigest()
-    
+    if(menu == '1'):
+        addnewTv()
+        print('----------------------')
+    elif(menu == '2'):
+        viewTv()
+        print('----------------------')
+    elif(menu == '3'):
+        selectTv()
+        print('----------------------')
+    elif(menu == '4'):
+        removeTv()
+        print('----------------------')
+    else:
+        print('-------- END --------')
+        break
 
-
-class Student(User):
-    def __init__(self) -> None:
-        super(Student).__init__()
-
-
-
-user1 = Student()
-
-
-
-user1._setPassword("assw")
-print(user1._comparePassword("assw"))
-
-
-
-print(user1.__dict__)
---
+++ readme.md
@@ -0,0 +1,13 @@
+Buatlah Program berdasakan Class dan object apa saja, dengan ketentuan:
+
+- Tidak boleh sama dengan teman
+- Menerapkan Inheritance (turunan)
+- Program harus dapat berjalan seperti object sesunguhnya
+- Membuat menu untuk menjalankan program
+
+Create a program based on any class and object (each student must different), There are some condition:
+
+- Can't be the same as friends
+- Implementing Inheritance
+- Programs must be able to perform similar behavior like the real object
+- Create a menu to start the program
